1c871dfcdcfe388126d45e4d6673b2096416f2f2
Victor Evangelista <victorevangelista@protonmail.com>
2019-12-13 09:24:26 -0800

    use extend instead of global comp registration

--- src/components/withSwiperSlide.js
+++ src/components/withSwiperSlide.js
@@ -1,11 +1,11 @@
-import Vue from 'vue';
+import Vue from "vue";
 
-const withSwiperSlide = (component) => {
-  return Vue.component('withSwiperSlide', {
+const withSwiperSlide = component => {
+  return Vue.extend("withSwiperSlide", {
     render(h) {
-      return h('li', { class: 'swiper-slide' }, component);
+      return h("li", { class: "swiper-slide" }, component);
     }
   });
-}
+};
 
 export default withSwiperSlide;
